0f43b30234fe3a336d105a1058123814fe55646c
Aditya Madhan <adityamkk@gmail.com>
2026-02-27 21:21:08 -0600

    feat: scalar tensor backprop

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
 pub mod nn;
+use nn::tensor::{Tensor, add, mul, backward};
+use std::rc::Rc;
+use std::cell::RefCell;
 
 fn main() {
     println!("Hello, world!");
+
+    let a = Rc::new(RefCell::new(Tensor::new(3.0)));
+    let x = Rc::new(RefCell::new(Tensor::new(4.0)));
+    let b = Rc::new(RefCell::new(Tensor::new(5.0)));
+
+    let z = add(&mul(&a, &x), &b);
+    backward(&z);
+
+    println!("A Data: {}, A Grad: {}", a.borrow().data, a.borrow().grad);
+    println!("X Data: {}, X Grad: {}", x.borrow().data, x.borrow().grad);
+    println!("B Data: {}, B Grad: {}", b.borrow().data, b.borrow().grad);
+    println!("Z Data: {}, Z Grad: {}", z.borrow().data, z.borrow().grad);
 }
--- src/nn/tensor.rs
+++ src/nn/tensor.rs
@@ -1,5 +1,6 @@
 use std::cell::RefCell;
 use std::rc::Rc;
+use std::collections::HashSet;
 
 type TensorRef = Rc<RefCell<Tensor>>;
 
@@ -8,4 +9,103 @@ pub struct Tensor {
     pub grad: f64,
     pub parents: Vec<TensorRef>,
     pub backward: Option<Box<dyn Fn()>>,
+}
+
+impl Tensor {
+    pub fn new(data: f64) -> Tensor {
+        Tensor {
+            data: data,
+            grad: 0.0,
+            parents: vec![],
+            backward: None
+        }
+    }
+}
+
+pub fn add(a: &TensorRef, b: &TensorRef) -> TensorRef {
+    let out = Rc::new(RefCell::new(Tensor {
+        data: a.borrow().data + b.borrow().data,
+        grad: 0.0,
+        parents: vec![a.clone(), b.clone()],
+        backward: None
+    }));
+
+    // Move cloned strong pointers into the backwards function
+    let out_clone = out.clone();
+    let a_clone = a.clone();
+    let b_clone = b.clone();
+
+    out.borrow_mut().backward = Some(Box::new(move || {
+        let grad = out_clone.borrow().grad;
+        a_clone.borrow_mut().grad += grad;
+        b_clone.borrow_mut().grad += grad;
+    }));
+
+    out
+}
+
+pub fn mul(a: &TensorRef, b: &TensorRef) -> TensorRef {
+    let out = Rc::new(RefCell::new(Tensor {
+        data: a.borrow().data * b.borrow().data,
+        grad: 0.0,
+        parents: vec![a.clone(), b.clone()],
+        backward: None
+    }));
+
+    // Move cloned strong pointers into the backwards function
+    let out_clone = out.clone();
+    let a_clone = a.clone();
+    let b_clone = b.clone();
+
+    out.borrow_mut().backward = Some(Box::new(move || {
+        let grad = out_clone.borrow().grad;
+        let a_data = a_clone.borrow().data;
+        let b_data = b_clone.borrow().data;
+        a_clone.borrow_mut().grad += grad * b_data;
+        b_clone.borrow_mut().grad += grad * a_data;
+    }));
+
+    out
+}
+
+/**
+ * Builds the topological sort of the computational graph
+ * Uses DFS
+ */
+pub fn topological_sort(t: &TensorRef, visited: &mut HashSet<usize>, order: &mut Vec<TensorRef>) {
+    let ptr = Rc::as_ptr(t) as usize; // This is just to get a quick hash of the tensor
+    if visited.contains(&ptr) {
+        // Already found this somewhere else in the dfs, so I'm already counting it
+        return
+    }
+    visited.insert(ptr);
+
+    // Do dfs on the parents
+    for parent in &t.borrow().parents {
+        topological_sort(parent, visited, order);
+    }
+
+    // Once all parents processed, I can finally add myself to the order
+    order.push(t.clone());
+}
+
+/**
+ * Given a Tensor to compute gradients on, compute gradients on all previous tensors
+ * Updates Gradients of Tensors in reverse topological order
+ * Gradients are summed up before being used to go backwards
+ */
+pub fn backward(t: &TensorRef) {
+    t.borrow_mut().grad = 1.0; // Initial gradient, eventually change this to some kind of loss function
+    
+    let mut order = Vec::<TensorRef>::new();
+    let mut visited = HashSet::<usize>::new();
+
+    topological_sort(t, &mut visited, &mut order);
+
+    // Iterate topological sort in reverse order to compute gradients
+    for tensor in order.into_iter().rev() {
+        if let Some(ref backward_fn) = tensor.borrow().backward {
+            backward_fn(); // Use current gradient to update gradients of parents
+        }
+    }
 }
